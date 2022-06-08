9bdd0109beb9defd5dd320dec435fb02bb1d69a5
לידור בן ישי <Lidor.BenYisha@ruppin365.net>
2022-06-08 19:20:37 +0300

    Add project files.

---
+++ Project18/BST.h
@@ -0,0 +1,29 @@
+#ifndef _BST_ADT_H
+#define _BST_ADT_H
+
+typedef struct TreeNode TreeNode;
+
+struct TreeNode
+{
+    TreeNode* left;
+    TreeNode* right;
+    int element;
+};
+
+typedef struct BST
+{
+    TreeNode* root;
+}BST;
+
+#endif // !_BST_ADT_H
+
+void initBST(BST* bst);
+TreeNode* createNode(int value);
+void insertBST(BST* bst, int value);
+void insertreeNode(TreeNode* root, TreeNode* newNode);
+void printTreeNodeInorder(TreeNode* root);
+void printTreeInorder(BST* bst);
+void destroyBST(BST* bst);
+int findIndexNFromLast(BST* bst, int N);
+int sameHeightLeaves(BST* bst);
+void print_ascii_tree(TreeNode* root);
---
+++ Project18/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "BST.h"
+#include "TreePrintLibrary.h"
+
+void main() {
+	BST bst;
+	BST *pbst= &bst;
+	int N =7;
+	initBST(&bst);
+	insertBST(&bst,4);
+	insertBST(&bst, 7);
+	insertBST(&bst, 3);
+	insertBST(&bst, 5);
+	insertBST(&bst, 9);
+	printTreeInorder(&bst);
+	int element= findIndexNFromLast(&bst, N);
+	printf("the element is:%d", element);
+	int num= sameHeightLeaves(&bst);
+	if (num = 0) {
+		printf("the number of leaves is different");
+	}
+	else
+		printf("the number of leaves is the same");
+
+	printTreeInorder(pbst);
+	print_ascii_tree(pbst);
+	destroyBST(pbst);
+}
